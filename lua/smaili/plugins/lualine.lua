2855bd524f74f366725401de913ccf824bd0f24d
MuhametSmaili <51492002+MuhametSmaili@users.noreply.github.com>
2023-04-25 14:08:09 +0300

    lualine improvment

--- lua/smaili/plugins/lualine.lua
+++ lua/smaili/plugins/lualine.lua
@@ -12,16 +12,26 @@ function M.config()
 	my_filename.apply_icon = require("lualine.components.filetype").apply_icon
 	my_filename.colored = true
 
+	local filename_with_path = { my_filename, path = 1, symbols = { modified = " ", readonly = "", unnamed = " " } }
+
 	require("lualine").setup({
 		options = {
 			icons_enabled = true,
-			globalstatus = true,
+			-- globalstatus = true, -- to have just one lualine, the inactive_* won't work
 			component_separators = "|",
 			section_separators = "",
 		},
 		sections = {
-			lualine_c = {
-				{ my_filename, path = 1, symbols = { modified = " ", readonly = "", unnamed = " " } },
+			lualine_c = { filename_with_path },
+		},
+		inactive_sections = {
+			lualine_a = { "filename" },
+			lualine_c = { filename_with_path },
+			lualine_y = {
+				{
+					"datetime",
+					style = "default", -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+				},
 			},
 		},
 		extensions = { "nvim-tree" },
