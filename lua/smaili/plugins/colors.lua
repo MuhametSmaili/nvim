6aa87209ecf0e14ed8772df401eff607afba06a9
MuhametSmaili <51492002+MuhametSmaili@users.noreply.github.com>
2023-05-04 07:23:02 +0300

    colorscheme update

--- lua/smaili/plugins/colors.lua
+++ lua/smaili/plugins/colors.lua
@@ -1,18 +1,16 @@
-------------------------------
 -- Theme
+------------------------------
 ------------------------------
 local M = {
 	{
 		"maxmx03/dracula.nvim",
-		lazy = false,
-		priority = 1000,
 		config = function()
+			-- Getting custom colors
 			local dracula = require("dracula")
 			local colors = require("dracula.palettes")
 			local setup_options = require("user.colors")
-
 			dracula.setup(setup_options.dracula(colors))
-			vim.cmd("colorscheme dracula")
+			-- vim.cmd("colorscheme dracula")
 		end,
 	},
 	{
@@ -28,12 +26,27 @@ local M = {
 		opts = { transparent = true },
 	},
 	{
+		lazy = false,
+		priority = 1000,
 		"catppuccin/nvim",
-		event = "VeryLazy",
 		name = "catppuccin",
 		opts = {
 			transparent_background = true,
+			highlight_overrides = {
+				all = function(colors)
+					return {
+						LineNr = { fg = "#4b5481" },
+						-- IllumniatedWordRead = { link = "LspReferenceRead " }, -- alternative to this color #426646
+						-- IllumniatedWordText = { link = "LspReferenceText " },
+						-- IllumniatedWordWrite = { link = "LspReferenceWrite " },
+					}
+				end,
+			},
 		},
+		config = function(plugin)
+			require("catppuccin").setup(plugin.opts)
+			vim.cmd("colorscheme catppuccin")
+		end,
 	},
 }
 
