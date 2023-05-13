7f2d89263ad18bd956bd007f3c7608207bd0904b
MuhametSmaili <51492002+MuhametSmaili@users.noreply.github.com>
2023-05-13 18:25:57 +0300

    refactoring code and removed dap

--- lua/smaili/plugins/nvim-ufo.lua
+++ lua/smaili/plugins/nvim-ufo.lua
@@ -1,28 +1,37 @@
-local M = {
+return {
 	"kevinhwang91/nvim-ufo",
 	keys = { { "za" } },
 	dependencies = {
 		"kevinhwang91/promise-async",
+		{
+			"luukvbaal/statuscol.nvim",
+			config = function()
+				local builtin = require("statuscol.builtin")
+				require("statuscol").setup({
+					relculright = true,
+					segments = {
+						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+						{ text = { "%s" }, click = "v:lua.ScSa" },
+						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					},
+				})
+			end,
+		},
 	},
+	config = function()
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "treesitter", "indent" }
+			end,
+		})
+	end,
 }
-
-function M.config()
-	vim.o.foldcolumn = "1" -- '0' is not bad
-	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-	vim.o.foldlevelstart = 99
-	vim.o.foldenable = true
-	vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
-	-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-	-- vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-	-- vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-	--
-	require("ufo").setup({
-		provider_selector = function(bufnr, filetype, buftype)
-			return { "treesitter", "indent" }
-		end,
-	})
-	--
-end
-
-return M
