c98472cc24966d060b29226195b29c507b4a032e
Muhamet Smaili <muhamet5@msn.com>
2023-12-16 02:13:27 +0200

    disable dashboarrd-nvim

--- lua/plugins/disable-plugins.lua
+++ lua/plugins/disable-plugins.lua
@@ -5,4 +5,5 @@ return {
   -- { "mini.indentscope", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
 }
