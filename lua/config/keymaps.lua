e476e3d4af88acf03aaf5412455289b92f4f7fb7
Muhamet Smaili <muhamet5@msn.com>
2023-12-16 02:07:31 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,21 @@ map("n", "<leader>w", "<cmd>w<cr>") -- save buffer
 -- map({ "n", "v" }, "<leader>lf", function()
 --   require("lazyvim.util").format({ force = true })
 -- end, { desc = "Format" })
+--
+-- n = {
+-- 	["<leader>gd"] = { gitsigns.diffthis, desc = "View git diff" },
+-- 	["<leader>ga"] = { gitsigns.get_actions, desc = "Get git actions" },
+-- 	["gb"] = { gitsigns.blame_line, desc = "View git blame" },
+-- 	["<leader>gR"] = { gitsigns.show, desc = "View git revision" },
+-- 	["<leader>gh"] = { gitsigns.preview_hunk, desc = "View git hunk per current line" },
+-- 	["]h"] = { gitsigns.next_hunk, desc = "Next hunk" },
+-- 	["[h"] = { gitsigns.prev_hunk, desc = "Prev hunk" },
+-- 	["<leader>gr"] = { gitsigns.reset_hunk, desc = "Reset current hunk" },
+-- 	["<leader>gH"] = { gitsigns.get_hunks, desc = "Get git hunks" },
+-- 	["<leader>gtd"] = { gitsigns.toggle_deleted, desc = "Git toggle deleted" },
+-- 	["<leader>gts"] = { gitsigns.toggle_signs, desc = "Git toggle signs" },
+-- 	["<leader>gtw"] = { gitsigns.toggle_word_diff, desc = "Git toggle word diff" },
+-- 	["<leader>gc"] = { ":FzfLua git_commits<cr>", desc = "View git commits for this buffer" },
+-- 	["<leader>gs"] = { ":FzfLua git_status<cr>", desc = "Git status" },
+-- 	["<leader>gb"] = { ":FzfLua git_bcommits<cr>", desc = "Git buffer commits" },
+-- },
