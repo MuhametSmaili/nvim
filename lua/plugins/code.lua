72b9aeb512f4a148eae727ea44f24ecf11334fd5
Muhamet Smaili <muhamet5@msn.com>
2023-12-16 02:06:45 +0200

    add more plugins

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -6,6 +6,8 @@ return {
       "tpope/vim-repeat",
     },
   },
+
+  -- vim-system-copy
   {
     "christoomey/vim-system-copy",
     keys = {
@@ -13,10 +15,52 @@ return {
       { "cv", desc = "Paste from system clipboard(motion)" },
     },
   },
+
+  -- UndotreeToggle
   {
     "mbbill/undotree",
     keys = {
       { "<leader>gu", "<cmd>UndotreeToggle<CR>", desc = "Toggle undotree" },
     },
+  },
+
+  -- Generate documentation
+  {
+    "danymat/neogen",
+    config = true,
+    keys = {
+      {
+        "<leader>ln",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Generate func|class|type documentation",
+      },
+    },
+  },
+
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+
+  -- Refactoring tool
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
   },
 }
