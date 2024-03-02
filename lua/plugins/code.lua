f6e64669879d0bc0281b3c4dcea9123a037e474d
Muhamet Smaili <muhamet5@msn.com>
2024-03-02 11:14:46 +0200

    added grapple and portal

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -78,75 +78,152 @@ return {
     },
     opts = {},
   },
-  -- harpoon (because of Prime)
   {
-    "ThePrimeagen/harpoon",
-    -- branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    "cbochs/grapple.nvim",
+    dependencies = {
+      {
+        "cbochs/portal.nvim",
+        keys = {
+          { "<leader>o", "<cmd>Portal jumplist backward<cr>", desc = "Jump backward" },
+          { "<leader>i", "<cmd>Portal jumplist forward<cr>", desc = "Jump forward" },
+          {
+            "<leader>j",
+            function()
+              require("portal.builtin").grapple.tunnel()
+            end,
+            desc = "Jump on grapple",
+          },
+        },
+      },
+    },
     keys = {
       {
         "<leader>a",
         function()
-          require("harpoon.mark").add_file()
+          require("grapple").toggle()
         end,
         desc = "Add file to harpoon",
       },
-      {
-        "<leader>n",
-        function()
-          require("harpoon.ui").nav_next()
-        end,
-        desc = "Go to next mark : harpoon list",
-      },
-      {
-        "<leader>p",
-        function()
-          require("harpoon.ui").nav_prev()
-        end,
-        desc = "Go to prev mark : harpoon list",
-      },
-      {
-        "<leader>p",
-        function()
-          require("harpoon.ui").nav_prev()
-        end,
-        desc = "Go to prev mark : harpoon list",
-      },
+      -- {
+      --   "<leader>n",
+      --   function()
+      --     require("harpoon.ui").nav_next()
+      --   end,
+      --   desc = "Go to next mark : harpoon list",
+      -- },
+      -- {
+      --   "<leader>p",
+      --   function()
+      --     require("harpoon.ui").nav_prev()
+      --   end,
+      --   desc = "Go to prev mark : harpoon list",
+      -- },
+      -- {
+      --   "<leader>p",
+      --   function()
+      --     require("harpoon.ui").nav_prev()
+      --   end,
+      --   desc = "Go to prev mark : harpoon list",
+      -- },
       {
         "<leader>1",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "Go to 1 file in harpoon list",
+        "<cmd>Grapple select index=1<cr>",
+        desc = "Go to 1 file in grapple list",
       },
       {
         "<leader>2",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "Go to 2 file in harpoon list",
+        "<cmd>Grapple select index=2<cr>",
+        desc = "Go to 2 file in grapple list",
       },
       {
         "<leader>3",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "Go to 3 file in harpoon list",
+        "<cmd>Grapple select index=3<cr>",
+        desc = "Go to 3 file in grapple list",
       },
       {
         "<leader>4",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "Go to 4 file in harpoon list",
+        "<cmd>Grapple select index=4<cr>",
+        desc = "Go to 4 file in grapple list",
       },
       {
         "<c-e>",
         function()
-          require("harpoon.ui").toggle_quick_menu()
+          require("grapple").open_tags()
         end,
-        desc = "Toggle harpoon UI quick/menu",
+        desc = "Toggle grapple UI quick/menu",
       },
     },
   },
+  -- harpoon (because of Prime)
+  -- {
+  --   -- cbochs/grapple.nvim
+  --   "ThePrimeagen/harpoon",
+  --   -- branch = "harpoon2",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   keys = {
+  --     {
+  --       "<leader>a",
+  --       function()
+  --         require("harpoon.mark").add_file()
+  --       end,
+  --       desc = "Add file to harpoon",
+  --     },
+  --     {
+  --       "<leader>n",
+  --       function()
+  --         require("harpoon.ui").nav_next()
+  --       end,
+  --       desc = "Go to next mark : harpoon list",
+  --     },
+  --     {
+  --       "<leader>p",
+  --       function()
+  --         require("harpoon.ui").nav_prev()
+  --       end,
+  --       desc = "Go to prev mark : harpoon list",
+  --     },
+  --     {
+  --       "<leader>p",
+  --       function()
+  --         require("harpoon.ui").nav_prev()
+  --       end,
+  --       desc = "Go to prev mark : harpoon list",
+  --     },
+  --     {
+  --       "<leader>1",
+  --       function()
+  --         require("harpoon.ui").nav_file(1)
+  --       end,
+  --       desc = "Go to 1 file in harpoon list",
+  --     },
+  --     {
+  --       "<leader>2",
+  --       function()
+  --         require("harpoon.ui").nav_file(2)
+  --       end,
+  --       desc = "Go to 2 file in harpoon list",
+  --     },
+  --     {
+  --       "<leader>3",
+  --       function()
+  --         require("harpoon.ui").nav_file(3)
+  --       end,
+  --       desc = "Go to 3 file in harpoon list",
+  --     },
+  --     {
+  --       "<leader>4",
+  --       function()
+  --         require("harpoon.ui").nav_file(4)
+  --       end,
+  --       desc = "Go to 4 file in harpoon list",
+  --     },
+  --     {
+  --       "<c-e>",
+  --       function()
+  --         require("harpoon.ui").toggle_quick_menu()
+  --       end,
+  --       desc = "Toggle harpoon UI quick/menu",
+  --     },
+  --   },
+  -- },
 }
