2fb364e0c34ce89bd45f8993e06762ce318d95c4
Muhamet Smaili <muhamet5@msn.com>
2024-04-22 22:28:12 +0300

    added debug-print nvim

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -174,4 +174,21 @@ return {
       },
     },
   },
+  {
+    "andrewferrier/debugprint.nvim",
+    opts = {},
+    -- keys = {
+    --   { "g?v", mode = { "n", "x" }, desc = "Veriable log" },
+    --   { "g?V", mode = { "n", "x" }, desc = "Veriable log above" },
+    --   { "g?p", mode = { "n", "x" }, desc = "Plain debug log below" },
+    --   { "g?P", mode = { "n", "x" }, desc = "Plain debug log below" },
+    -- },
+    dependencies = {
+      -- "echasnovski/mini.nvim", -- Needed to enable :ToggleCommentDebugPrints for NeoVim <= 0.9
+      -- "nvim-treesitter/nvim-treesitter", -- Needed to enable treesitter for NeoVim 0.8
+    },
+    -- Remove the following line to use development versions,
+    -- not just the formal releases
+    version = "*",
+  },
 }
