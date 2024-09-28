e5e58c6de1e90934906ad985b0484918a68db139
Muhamet Smaili <muhamet5@msn.com>
2024-09-28 08:21:52 +0300

    added more options for vtsls (trying)

--- lua/custom/plugins/lsp/servers/vtsls.lua
+++ lua/custom/plugins/lsp/servers/vtsls.lua
@@ -12,8 +12,17 @@ return {
 		},
 		typescript = {
 			updateImportsOnFileMove = { enabled = "always" },
+			-- organizeImportsOnSave = true, -- Automatically organize imports on save
+			-- removeUnusedImports = true, -- Automatically remove unused imports
 			suggest = {
 				completeFunctionCalls = true,
+				includeCompletionsForModuleExports = true, -- Suggest exports from other modules
+				includeAutomaticOptionalChainCompletions = true, -- Auto-complete with optional chaining where applicable
+			},
+			compilerOptions = {
+				strict = true, -- Enable strict type checks
+				strictNullChecks = true, -- Enforce null safety
+				strictPropertyInitialization = true, -- Ensure all properties are initialized
 			},
 			inlayHints = {
 				enumMemberValues = { enabled = true },
@@ -22,6 +31,11 @@ return {
 				parameterTypes = { enabled = true },
 				propertyDeclarationTypes = { enabled = true },
 				variableTypes = { enabled = false },
+			},
+			diagnostics = {
+				diagnosticsDelay = "500ms", -- diagnostic feedback
+				enableUnused = true, -- Highlight unused variables
+				enableUnreachableCode = true, -- Highlight unreachable code
 			},
 		},
 	},
