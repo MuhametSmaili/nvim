691a69ee4e3f374f843eb449f482adbf1ebfda5a
Muhamet Smaili <muhamet5@msn.com>
2025-05-01 21:15:13 +0300

    add snacks extension for lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,63 +2,53 @@
 -- name : nvim-lualine
 -- url  : https://github.com/nvim-lualine/lualine.nvim
 -------------------------------------------------
-local M = {
+return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
-}
+	opts = function()
+		local my_filename = require("lualine.components.filename"):extend()
+		my_filename.apply_icon = require("lualine.components.filetype").apply_icon
+		my_filename.colored = true
 
-function M.config()
-	local my_filename = require("lualine.components.filename"):extend()
-	my_filename.apply_icon = require("lualine.components.filetype").apply_icon
-	my_filename.colored = true
+		local filename_with_path =
+			{ my_filename, path = 1, symbols = { modified = " ", readonly = "", unnamed = " " } }
 
-	local filename_with_path = { my_filename, path = 1, symbols = { modified = " ", readonly = "", unnamed = " " } }
-
-	require("lualine").setup({
-		options = {
-			theme = "catppuccin",
-			icons_enabled = true,
-			-- globalstatus = true, -- to have just one lualine, the inactive_* won't work
-			component_separators = "|",
-			section_separators = "",
-		},
-		sections = {
-			lualine_c = { filename_with_path },
-			lualine_b = { "grapple" },
-			lualine_x = {
-				"location",
-				-- "fileformat",
-				"filetype",
-				{
-					"lsp_status",
-					-- icon = require("lualine.components.filetype").apply_icon, -- f013
-					symbols = {
-						-- Standard unicode symbols to cycle through for LSP progress:
-						spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-						-- Standard unicode symbol for when LSP is done:
-						done = "✓",
-						-- Delimiter inserted between LSP names:
-						separator = " ",
+		return {
+			options = {
+				theme = "catppuccin",
+				icons_enabled = true,
+				component_separators = "",
+				section_separators = "",
+			},
+			sections = {
+				lualine_b = {
+					"grapple",
+				},
+				lualine_c = { filename_with_path },
+				lualine_x = {
+					"filetype",
+					{
+						"lsp_status",
+						symbols = {
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							done = "✓",
+							separator = " ",
+						},
+					},
+					"diagnostics",
+				},
+			},
+			inactive_winbar = {
+				lualine_a = { "filename" },
+				lualine_c = { filename_with_path },
+				lualine_y = {
+					{
+						"datetime",
+						style = "default", -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
 					},
 				},
 			},
-
-			-- lualine_x = {
-			-- "location",
-			-- },
-		},
-		inactive_winbar = {
-			lualine_a = { "filename" },
-			lualine_c = { filename_with_path },
-			lualine_y = {
-				{
-					"datetime",
-					style = "default", -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-				},
-			},
-		},
-		extensions = { "nvim-tree", "fzf" },
-	})
-end
-
-return M
+			extensions = { "nvim-tree", "fzf", "lazy", "fugitive", require("lua.custom.utils.snacks_lualine") },
+		}
+	end,
+}
