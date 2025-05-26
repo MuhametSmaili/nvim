9bd745d28a7b0b568d87a7847c6fbacd131508ba
Muhamet Smaili <muhamet5@msn.com>
2025-05-26 09:23:07 +0300

    improve arrow.nvim line component

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,40 @@
 -- name : nvim-lualine
 -- url  : https://github.com/nvim-lualine/lualine.nvim
 -------------------------------------------------
+local function arrow_all_buffers()
+	local arrow = require("arrow.statusline")
+	local filenames = vim.g.arrow_filenames or {}
+	local current_index = arrow.is_on_arrow_file()
+
+	if #filenames == 0 then
+		return ""
+	end
+
+	local parts = {}
+	table.insert(parts, "󱡁 ")
+
+	for i, _ in ipairs(filenames) do
+		local label = arrow.text_for_statusline(_, i)
+		local highlighted_label
+
+		if i == current_index then
+			highlighted_label = "%#ArrowCurrentFile#[" .. label .. "]"
+		else
+			highlighted_label = "%#ArrowFileIndex#" .. label
+		end
+
+		table.insert(parts, highlighted_label)
+
+		if i < #filenames then
+			table.insert(parts, "%*|")
+		end
+	end
+
+	table.insert(parts, "%*")
+
+	return table.concat(parts)
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
@@ -13,19 +47,6 @@ return {
 		local filename_with_path =
 			{ my_filename, path = 1, symbols = { modified = " ", readonly = "", unnamed = " " } }
 
-		local function arrow()
-			local status = require("arrow.statusline").text_for_statusline_with_icons()
-			return status
-		end
-
-		local arrow_module = {
-			arrow,
-		}
-		local statusline = require("arrow.statusline")
-		arrow_index = statusline.text_for_statusline(_, i)
-		print(index)
-		-- local arrow_filenames = vim.g.arrow_filenames
-
 		return {
 			options = {
 				theme = "auto",
@@ -34,8 +55,20 @@ return {
 				section_separators = "",
 			},
 			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							local m = vim.api.nvim_get_mode().mode
+							if m == "\22" then
+								return "C-V"
+							end
+							return str:sub(1, 1)
+						end,
+					},
+				},
 				lualine_b = {
-					arrow_module,
+					{ arrow_all_buffers },
 				},
 				lualine_c = { filename_with_path },
 				lualine_x = {
