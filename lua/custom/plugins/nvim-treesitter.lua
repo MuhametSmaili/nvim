a08b2671d5c258db30cac0e8c14ea96ebbf370d6
Muhamet Smaili <muhamet5@msn.com>
2025-06-03 10:24:16 +0300

    update treesitter

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -6,17 +6,15 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	event = "BufReadPost",
+	branch = "main",
 	dependencies = {
 		{
 			"nvim-treesitter/nvim-treesitter-context",
 			config = true,
 			opts = {
-				enable = false,
-				-- Avoid the sticky context from growing a lot.
+				enable = true,
 				max_lines = 3,
-				-- Match the context lines to the source code.
 				multiline_threshold = 1,
-				-- Disable it when the window is too small.
 				min_window_height = 20,
 			},
 
@@ -46,6 +44,7 @@ return {
 			"regex",
 			"html",
 			"css",
+			"scss",
 			"jsdoc",
 			"astro",
 			"go",
@@ -59,53 +58,57 @@ return {
 			"vimdoc",
 			"gitignore",
 			"gitcommit",
+			"git_config",
 			"diff",
+			"http",
 			"git_rebase",
 			"toml",
 			"yaml",
 		},
-		highlight = {
-			enable = true,
-			disable = { "bash" },
-			additional_vim_regex_highlighting = false,
-		},
-		indent = {
-			enable = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = "<c-s>",
-				node_decremental = "<c-backspace>",
-			},
-		},
-		query_linter = {
-			enable = true,
-			use_virtual_text = true,
-			-- lint_events = { "BufWrite", "CursorHold" },
-		},
-		playground = {
-			enable = true,
-			disable = {},
-			updatetime = 25,
-			persist_queries = false,
-			keybindings = {
-				toggle_query_editor = "o",
-				toggle_hl_groups = "i",
-				toggle_injected_languages = "t",
-				toggle_anonymous_nodes = "a",
-				toggle_language_display = "I",
-				focus_language = "f",
-				unfocus_language = "F",
-				update = "R",
-				goto_node = "<cr>",
-				show_help = "?",
-			},
-		},
 	},
 	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
+		require("nvim-treesitter").setup()
+		require("nvim-treesitter").install(opts.ensure_installed)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"javascript",
+				"typescript",
+				"typescriptreact",
+				"lua",
+				"json",
+				"prisma",
+				"sql",
+				"regex",
+				"html",
+				"css",
+				"scss",
+				"astro",
+				"go",
+				"gomod",
+				"sh",
+				"markdown",
+				"markdown_inline",
+				"http",
+				"query",
+				"vim",
+				"vimdoc",
+				"gitignore",
+				"gitcommit",
+				"gitconfig",
+				"diff",
+				"gitrebase",
+				"toml",
+				"yaml",
+				"python",
+			},
+			callback = function()
+				local ok = pcall(vim.treesitter.start)
+				if not ok then
+					print("Treesitter not enabling")
+					return
+				end
+			end,
+		})
 	end,
 }
